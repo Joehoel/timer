dd4e112f49cfc63f63afbed9d1147265b9ab9e59
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-07-13 13:20:21 +0200

    Made it look better and added new options

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
-const timer = document.getElementById("timer");
-let countdown;
+const timer = document.getElementById("timer")
+const buttons = document.querySelectorAll("[data-time]")
+let countdown
 
-function startTimer() {
-  clearInterval(countdown);
+function startTimer(seconds) {
+	clearInterval(countdown)
+	display(seconds)
 
-  const now = Date.now();
-  const then = now + 1500 * 1000;
+	const now = Date.now()
+	const then = now + seconds * 1000
 
-  countdown = setInterval(() => {
-    
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    
-    // Check if we should stop it!
-    if (secondsLeft <= 0) {
-      
-      clearInterval(countdown);
-      
-      return false;
-    }
+	countdown = setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now()) / 1000)
+		// Check if we should stop it!
+		if (secondsLeft <= 0) {
+			clearInterval(countdown)
 
-    // eslint-disable-next-line no-use-before-define
-    display(secondsLeft);
-  }, 1000);
+			return false
+		}
+
+		// eslint-disable-next-line no-use-before-define
+		display(secondsLeft)
+	}, 1000)
 }
 
 function display(seconds) {
-  
-  const minutes = Math.floor(seconds / 60);
-  const remainderSeconds = seconds % 60;
-  
-  const timeLeft = `${minutes}:${
-    remainderSeconds > 9 ? remainderSeconds : `0${remainderSeconds}`
-  }`;
+	const minutes = Math.floor(seconds / 60)
+	const remainderSeconds = seconds % 60
 
-  document.title = timeLeft;
-  timer.textContent = timeLeft;
-  
+	const timeLeft = `${minutes}:${
+		remainderSeconds > 9 ? remainderSeconds : `0${remainderSeconds}`
+	}`
+
+	document.title = timeLeft
+	timer.textContent = timeLeft
 }
 
 async function installServiceWorkerAsync() {
-  
-  if ("serviceWorker" in navigator) {
-    try {
-      const serviceWorker = await navigator.serviceWorker.register("/sw.js");
-    } catch (error) {
-      console.error(`Failed to register service worker: ${error}`);
-    }
-  }
-  
+	if ("serviceWorker" in navigator) {
+		try {
+			const serviceWorker = await navigator.serviceWorker.register(
+				"/sw.js"
+			)
+		} catch (error) {
+			console.error(`Failed to register service worker: ${error}`)
+		}
+	}
 }
 
-window.addEventListener("load", installServiceWorkerAsync);
-document.body.addEventListener("click", startTimer);
-
+buttons.forEach(button =>
+	button.addEventListener("click", () => {
+		startTimer(button.dataset.time * 60)
+	})
+)
+window.addEventListener("load", () => {
+	display(timer.dataset.time * 60)
+	// installServiceWorkerAsync()
+})
