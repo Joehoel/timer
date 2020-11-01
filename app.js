ce79f172ecb8523b903613e0110b37353291f1f2
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-11-01 17:13:48 +0100

    Added audio

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-const timer = document.getElementById("timer")
-const buttons = document.querySelectorAll("[data-time]")
-let countdown
+const timer = document.getElementById("timer");
+const buttons = document.querySelectorAll("[data-time]");
+const alarm = document.querySelector("#alarm");
+let countdown;
 
 function startTimer(seconds) {
-	clearInterval(countdown)
-	display(seconds)
+	clearInterval(countdown);
+	alarm.pause();
+	display(seconds);
 
-	const now = Date.now()
-	const then = now + seconds * 1000
+	const now = Date.now();
+	const then = now + seconds * 1000;
 
 	countdown = setInterval(() => {
-		const secondsLeft = Math.round((then - Date.now()) / 1000)
+		const secondsLeft = Math.round((then - Date.now()) / 1000);
 		// Check if we should stop it!
-		if (secondsLeft <= 0) {
-			clearInterval(countdown)
+		display(secondsLeft);
 
-			return false
+		if (secondsLeft <= 0) {
+			clearInterval(countdown);
+
+			timer.textContent = "Reset";
+			alarm.loop = true;
+			alarm.play();
+
+			return false;
 		}
 
 		// eslint-disable-next-line no-use-before-define
-		display(secondsLeft)
-	}, 1000)
+	}, 1000);
 }
 
 function display(seconds) {
-	const minutes = Math.floor(seconds / 60)
-	const remainderSeconds = seconds % 60
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
 
 	const timeLeft = `${minutes}:${
 		remainderSeconds > 9 ? remainderSeconds : `0${remainderSeconds}`
-	}`
+	}`;
 
-	document.title = timeLeft
-	timer.textContent = timeLeft
+	document.title = timeLeft;
+	timer.textContent = timeLeft;
 }
 
 async function installServiceWorkerAsync() {
@@ -40,19 +47,20 @@ async function installServiceWorkerAsync() {
 		try {
 			const serviceWorker = await navigator.serviceWorker.register(
 				"/sw.js"
-			)
+			);
 		} catch (error) {
-			console.error(`Failed to register service worker: ${error}`)
+			console.error(`Failed to register service worker: ${error}`);
 		}
 	}
 }
 
 buttons.forEach(button =>
 	button.addEventListener("click", () => {
-		startTimer(button.dataset.time * 60)
+		startTimer(button.dataset.time * 60);
 	})
-)
+);
+
 window.addEventListener("load", () => {
-	display(timer.dataset.time * 60)
-	installServiceWorkerAsync()
-})
+	display(timer.dataset.time * 60);
+	installServiceWorkerAsync();
+});
